d64cf7842893a2aa662947d2f36b57042da9d656
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-13 22:13:34 +0200

    Use UserDefaults to load and save all data

--- HabitTracker/ActivitiesList.swift
+++ HabitTracker/ActivitiesList.swift
@@ -9,9 +9,23 @@
 import Foundation
 
 class ActivitiesList: ObservableObject {
-    @Published var items: [Activity]
+    @Published var items: [Activity] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
     
     init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([Activity].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
         self.items = []
     }
 }
